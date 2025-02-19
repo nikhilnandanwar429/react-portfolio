d61a3db8dc2897603d04616f14c4a34066efd8b8
Nikhil Nandanwar <nikhilnandanwar429@gmail.com>
2025-02-19 16:08:35 +0530

    fixes

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vitejs.dev/config/
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0'
-  }
+  plugins: [
+    react(),
+    tailwindcss(),
 
+  ],
 })
